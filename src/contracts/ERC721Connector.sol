63481fa5db2a615b1b89df2f6bc387085d007790
Kong-E <kng001016@dgu.ac.kr>
2023-07-31 14:29:04 +0900

    feat: ERC721 토큰 존재 여부, 민트 함수 추가

--- src/contracts/ERC721Connector.sol
+++ src/contracts/ERC721Connector.sol
@@ -4,6 +4,8 @@ pragma solidity >=0.4.22 <0.9.0;
 import "./ERC721Metadata.sol";
 
 contract ERC721Connector is ERC721Metadata {
+
+  // we want to carry the metadata info over
   constructor(string memory named, string memory symbolified) ERC721Metadata(named, symbolified) public {
   }
 }
