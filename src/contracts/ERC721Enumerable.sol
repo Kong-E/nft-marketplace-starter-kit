c42b41a1142ba5837401ad3b0996f5af84dc4761
Kong-E <kng001016@dgu.ac.kr>
2023-07-31 20:13:59 +0900

    chore: 컴파일 에러 해결 및 각주 추가

--- src/contracts/ERC721Enumerable.sol
+++ src/contracts/ERC721Enumerable.sol
@@ -16,16 +16,16 @@ contract ERC721Enumerable is ERC721{
   // mapping from token ID index of the owner token ids
   mapping(uint256 => uint256) private _ownedTokensIndex;
   
-    function _mint(address to, uint256 tokenId) internal override(ERC721) {
-      super._mint(to, tokenId);
-      _addTokenToTotalSupply(tokenId);
-    }
+  function _mint(address to, uint256 tokenId) internal override(ERC721) {
+    super._mint(to, tokenId);
+    _addTokenToTotalSupply(tokenId);
+  } // 민트된 토큰은 토탈서플라이에 추가
 
-    function _addTokenToTotalSupply(uint256 tokenId) private {
-      _allTokens.push(tokenId);
-    }
+  function _addTokenToTotalSupply(uint256 tokenId) private {
+    _allTokens.push(tokenId);
+  } // 토탈서플라이에 토큰 추가
 
-    function totalSupply() public view returns (uint256) {
-      return _allTokens.length;
-    }
+  function totalSupply() public view returns (uint256) {
+    return _allTokens.length;
+  } // 토탈서플라이 길이 반환
 }
